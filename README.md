5945204034bb88cb6aa50647e76fa7be9fb24d1c
Maksym Maksymenko <finside41@gmail.com>
2019-08-25 23:08:36 +0300

    add form 0.5.0 with fetch

--- README.md
+++ README.md
@@ -5,16 +5,15 @@ This is a simple project which is basically a registration form. The general ide
 ## Getting started
 
 - Run `npm i && npm i -g json-server`
-- Run `npm run dev` to start a client development server. By default available at `0.0.0.0:3001`
-- Run `node server/app.js` to start a backend server. By default available at `0.0.0.0:3002`
-- [Get a mockup](zpl.io/adzK17p). If you don't have access, please write a mail to artem@24slides.com
+- Run `npm run dev` to start a client development server. By default available at `127.0.0.2:3001`
+- Run `node server/app.js` to start a backend server. By default available at `127.0.0.1:3002`
 - If you want to make a build, run `npm run build`
 
 ## Requirements
 
 - Create a markup of the page using SASS
 - Create React application (on top of Redux) that simply registers a user
-- The form should have validation. Users should be registered via server through the endpoint **POST 0.0.0.0:3002/register**
-- Countries should be grabbed from **GET 0.0.0.0:3002/countries**
+- The form should have validation. Users should be registered via server through the endpoint **POST 127.0.0.1:3002/register**
+- Countries should be grabbed from **GET 127.0.0.1:3002/countries**
 
 That's it. Enjoy :)
--- scripts/components/Form/Form.jsx
+++ scripts/components/Form/Form.jsx
@@ -1,169 +1,378 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 import { getCountries } from "./get-data-from-server";
+import { log } from "util";
 
 class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error_message: true
+      error_message: false,
+      form_sending: false,
+      succsess: false,
+      errors: [],
+      length: 0
     };
+
     this.selectCountriesRef = React.createRef();
+    this.inputNameRef = React.createRef();
+    this.selectCodeRef = React.createRef();
+    this.inputNumberRef = React.createRef();
+    this.inputEmailRef = React.createRef();
+    this.inputPasswordRef = React.createRef();
+    this.inputPasswordConfirmRef = React.createRef();
+    this.inputCheckboxConfirmRef = React.createRef();
   }
 
   async componentDidMount() {
     const countries = await getCountries();
 
     this.setState({
-      isLoading: true,
+      isLoaded: true,
       countries
     });
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    // if (
+    //   this.inputCheckboxConfirmRef.current.checked === true &&
+    //   this.inputNameRef.current.value &&
+    //   this.selectCodeRef.current.value &&
+    //   this.inputNumberRef.current.value &&
+    //   this.inputEmailRef.current.value &&
+    //   this.inputPasswordRef.current.value &&
+    //   this.inputPasswordConfirmRef.current.value &&
+    //   this.selectCountriesRef.current.value &&
+    //   this.inputPasswordRef.current.value ===
+    //     this.inputPasswordConfirmRef.current.value
+    // ) {
+    this.setState({
+      form_sending: true
+    });
+
+    let newUser = {
+      name: this.inputNameRef.current.value,
+      dialCode: this.selectCodeRef.current.value,
+      email: this.inputEmailRef.current.value,
+      country: this.selectCountriesRef.current.value,
+      password: this.inputPasswordRef.current.value,
+      passwordConfirmation: this.inputPasswordConfirmRef.current.value
+    };
+
+    fetch("http://127.0.0.1:3002/register", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        if (response.status === "success") {
+          this.setState({
+            succsess: true,
+            form_sending: false
+          });
+        }
+        if (response.errors) {
+          let errors = response.errors.map((error, index) => {
+            return error.param;
+          });
+          this.setState({
+            form_sending: false,
+            errors
+          });
+        }
+      })
+      .catch(error => console.error("Error:", error));
+  };
+
+  checkServerResponse = errors => {
+    console.log(errors);
+  };
+
+  handlePasswordValidation = () => {
+    if (
+      this.inputPasswordRef.current.value !==
+      this.inputPasswordConfirmRef.current.value
+    ) {
+      this.setState({
+        error_message: true
+      });
+    }
+
+    if (
+      this.inputPasswordRef.current.value ===
+      this.inputPasswordConfirmRef.current.value
+    ) {
+      this.setState({
+        error_message: false
+      });
+    }
+  };
+
+  hideSuccsessMessage = () => {
+    this.setState({
+      succsess: false
+    });
+  };
+
   render() {
-    const { error_message, isLoading, countries } = this.state;
+    const {
+      error_message,
+      isLoaded,
+      countries,
+      form_sending,
+      succsess,
+      errors,
+      length
+    } = this.state;
 
     return (
-      <div className="container">
-        <div className="container__title">Sign up</div>
-        <form action="#!" className="container__form form">
-          <label className="form__label" htmlFor="name">
-            Name
-          </label>
-          <span className="bar" />
-          <input
-            className="form__input"
-            type="text"
-            id="name"
-            required
-            autoComplete="off"
-            placeholder="Your name"
-          />
-          <label className="form__label" htmlFor="phone">
-            Phone
-          </label>
-          <div className="form__wrapper">
-            <select required className="form__select" defaultValue="null">
-              <option value="null" disabled>
-                Code
-              </option>
-              {isLoading && countries.length > 0
-                ? countries.map(country => {
-                    return (
-                      <option
-                        key={country.dial_code + country.name}
-                        value={country.dial_code}
-                      >
-                        {`+ ${country.dial_code}`} &nbsp; &nbsp; &nbsp;
-                        &nbsp;&nbsp; {`${country.name}`}
-                      </option>
-                    );
-                  })
-                : null}
-            </select>
+      <main className="main">
+        <Succsess_message succsess={succsess} hide={this.hideSuccsessMessage} />
+        <div className="container">
+          <div className="container__title">Sign up</div>
 
+          <form
+            onSubmit={e => this.handleSubmit(e)}
+            className="container__form form"
+          >
+            <label className="form__label" htmlFor="name">
+              Name
+            </label>
             <input
               className="form__input"
-              type="tel"
-              id="phone"
+              type="text"
+              id="name"
               autoComplete="off"
-              required
-              placeholder="Phone number"
+              placeholder="Your name"
+              ref={this.inputNameRef}
             />
-          </div>
-
-          <label className="form__label" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="form__input"
-            type="email"
-            id="email"
-            autoComplete="off"
-            required
-            placeholder="Email address"
-          />
-          <label className="form__label" htmlFor="country">
-            Country
-          </label>
-          <div className="form__wrapper" style={{ width: "100%" }}>
-            <select
-              className="form__select"
-              id="country"
-              required
-              placeholder="Select country"
-              ref={this.selectCountriesRef}
-              defaultValue="1"
-            >
-              <option className="form-option" value="1" disabled>
-                Select country
-              </option>
-              {isLoading && countries.length > 0
-                ? countries.map(country => {
-                    return (
-                      <option
-                        key={country.id + country.name}
-                        value={country.name}
-                      >
-                        {country.name}
-                      </option>
-                    );
-                  })
-                : null}
-            </select>
-          </div>
-          <label className="form__label" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="form__input"
-            type="password"
-            id="password"
-            autoComplete="off"
-            required
-            placeholder="Password"
-          />
-          <label className="form__label" htmlFor="password_conf">
-            Confirm password
-          </label>
-          <input
-            className={`form__input ${
-              error_message ? "form__input-not-valid" : "form__input-valid"
-            }`}
-            type="password"
-            id="password_conf"
-            autoComplete="off"
-            required
-            placeholder="Confirm your password"
-          />
-          <span
-            className={`form__error_message ${
-              error_message ? "form__error_message-shown" : ""
-            }`}
-          >
-            Your password do not match
-          </span>
-
-          <label className="form__label-check" htmlFor="checkbox">
+            <Error_message error_message={errors.includes("name")} />
+            <label className="form__label" htmlFor="phone">
+              Phone
+            </label>
+            <div className="form__wrapper">
+              <select
+                className="form__select"
+                defaultValue="null"
+                ref={this.selectCodeRef}
+              >
+                <option value="null" disabled>
+                  Code
+                </option>
+                {isLoaded && countries.length > 0
+                  ? countries.map(country => {
+                      return (
+                        <option
+                          key={country.dial_code + country.name}
+                          value={country.dial_code}
+                        >
+                          {`+ ${country.dial_code}`} &nbsp; &nbsp; &nbsp;
+                          &nbsp;&nbsp; {`${country.name}`}
+                        </option>
+                      );
+                    })
+                  : null}
+              </select>
+              <input
+                className="form__input"
+                type="tel"
+                id="phone"
+                autoComplete="off"
+                placeholder="Phone number"
+                ref={this.inputNumberRef}
+              />
+            </div>
+            <Error_message error_message={errors.includes("dialCode")} />
+            <label className="form__label" htmlFor="email">
+              Email
+            </label>
             <input
-              className="form-label__input"
-              type="checkbox"
-              id="checkbox"
-              required
+              className="form__input"
+              type="email"
+              id="email"
+              autoComplete="off"
+              placeholder="Email address"
+              ref={this.inputEmailRef}
             />
-            <span className="check__box" />
-            Yes, I'd like to recieve the very occasional email with information
-            on new services and discounts
-          </label>
-          <div className="form__button">create an account</div>
-          <p className="form__paragraph">
-            Already have a 24Slides account? Click here to log in to your
-            existing account and join a company team
-          </p>
-        </form>
-      </div>
+            <Error_message error_message={errors.includes("email")} />
+            <label className="form__label" htmlFor="country">
+              Country
+            </label>
+            <div className="form__wrapper">
+              <select
+                className="form__select"
+                id="country"
+                placeholder="Select country"
+                ref={this.selectCountriesRef}
+                defaultValue=""
+              >
+                <option className="form-option" value="" disabled>
+                  Select country
+                </option>
+                {isLoaded && countries.length > 0
+                  ? countries.map(country => {
+                      return (
+                        <option
+                          key={country.id + country.name}
+                          value={country.country_code}
+                        >
+                          {country.name} --- {country.country_code}
+                        </option>
+                      );
+                    })
+                  : null}
+              </select>
+            </div>
+            <Error_message
+              error_message={errors.includes("country")}
+              message="Invalid value (available only GB, US, UA)"
+            />
+            <label className="form__label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className={`form__input ${
+                error_message
+                  ? "form__input-not-valid"
+                  : length > 5
+                  ? "form__input-valid"
+                  : ""
+              }`}
+              type="password"
+              id="password"
+              autoComplete="off"
+              placeholder="Password"
+              ref={this.inputPasswordRef}
+              onChange={() => (this.state.length = this.inputPasswordRef)}
+            />
+            <Error_message error_message={errors.includes("password")} />
+            <label className="form__label" htmlFor="password_conf">
+              Confirm password
+            </label>
+            <input
+              className={`form__input ${
+                error_message
+                  ? "form__input-not-valid"
+                  : length > 5
+                  ? "form__input-valid"
+                  : ""
+              }`}
+              type="password"
+              id="password_conf"
+              autoComplete="off"
+              placeholder="Confirm your password"
+              ref={this.inputPasswordConfirmRef}
+              onChange={this.handlePasswordValidation}
+            />
+            <Error_message
+              error_message={error_message}
+              message={"Your password do not match"}
+            />
+            <label className="form__label-check" htmlFor="checkbox">
+              <input
+                className="form-label__input"
+                type="checkbox"
+                id="checkbox"
+                ref={this.inputCheckboxConfirmRef}
+              />
+              <span className="check__box" />
+              Yes, I'd like to recieve the very occasional email with
+              information on new services and discounts
+            </label>
+            <button type="submit" className="form__button">
+              {form_sending ? (
+                <span className="form__button-icon" />
+              ) : (
+                "create an account"
+              )}
+            </button>
+            <p className="form__paragraph">
+              Already have a 24Slides account?{" "}
+              <a className="form__paragraph-link" href="https://24slides.com/">
+                Click here
+              </a>{" "}
+              to log in to your existing account and join a company team
+            </p>
+            <br /> <br /> <br />
+          </form>
+          {/* <form action="#!" className="container__form form">
+            <label className="form__label" htmlFor="phone">
+              Phone
+            </label>
+
+            <input  className="form__input1" />
+
+            <div className="dial_list">
+              <div className="dial_item">
+                <div>123</div>
+                <div>Amsterdam</div>
+              </div>
+              <div className="dial_item">
+                <div>456</div>
+                <div className="country">Paris</div>
+              </div>
+              <div className="dial_item">
+                <div>987</div>
+                <div>London</div>
+              </div>
+            </div>
+          </form> */}
+        </div>
+      </main>
     );
   }
 }
+
+const Error_message = ({ error_message, message }) => {
+  return (
+    <span
+      className={`form__error_message ${
+        error_message ? "form__error_message-shown" : ""
+      }`}
+    >
+      {message}
+    </span>
+  );
+};
+
+Error_message.propTypes = {
+  error_message: PropTypes.bool.isRequired,
+  message: PropTypes.string
+};
+
+Error_message.defaultProps = {
+  message: "Invalid value"
+};
+
+const Succsess_message = ({ succsess, hideSuccsessMessage, message }) => {
+  return (
+    <div className={`main__success-message ${succsess ? "show" : ""}`}>
+      <div className="message__tittle">
+        <span className="message__tittle-icon" />
+        Great!
+        <span
+          onClick={hideSuccsessMessage}
+          className="message__tittle-close-icon"
+        />
+      </div>
+      <div className="message__message">{message}</div>
+    </div>
+  );
+};
+
+Succsess_message.propTypes = {
+  succsess: PropTypes.bool.isRequired,
+  hideSuccsessMessage: PropTypes.func
+};
+
+Succsess_message.defaultProps = {
+  message: "your account has been successfully created."
+};
 
 export default Form;
