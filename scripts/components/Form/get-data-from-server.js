24a8b6901a79442e219f56a7242ed494b1a0f59a
Maksym Maksymenko <finside41@gmail.com>
2019-08-27 23:45:36 +0300

    change url for codreview

--- scripts/components/Form/get-data-from-server.js
+++ scripts/components/Form/get-data-from-server.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = "http://127.0.0.1:3002/";
+export const BASE_URL = "http://127.0.0.1:3002/";
 
-export const BASE_URL = "https://reg-form-local-server.herokuapp.com/";
+// export const BASE_URL = "https://reg-form-local-server.herokuapp.com/";
 
 export const getCountriesFromServer = () => {
   return fetch(`${BASE_URL}countries`)
